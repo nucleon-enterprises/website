a537de36128da7a754c026f6f59d3f8fb0c3b02c
cl3t0 <cl3t0.sh@gmail.com>
2019-09-14 23:42:33 -0300

    a little change in the file structure

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React {this.state.response}</h1>
+          <h1 className="App-title">{this.state.response}</h1>
         </header>
       </div>
     );
